148095f2f6eb40995191aee836d1a93346391f64
XL_Mr <643158351@qq.com>
2022-06-10 22:11:51 +0800

    判断用户是否登录

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,19 @@ function getUserinfo() {
     headers: {
       Authorization: localStorage.getItem("token"),
     },
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    complete: (res) => {
+      // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+      if (
+        res.responseJSON.status === 1 &&
+        res.responseJSON.message === "身份认证失败！"
+      ) {
+        //  强制清空 token
+        localStorage.removeItem("token");
+        // 强制跳转到登录页面
+        location.href = "/login.html";
+      }
+    },
     success: (res) => {
       console.log(res);
       if (res.status !== 0) {
@@ -21,7 +34,7 @@ const renderAvatar = (user) => {
   const name = user.nickname || user.username;
   // console.log(name);
   // 渲染头像
-  $("#welcome").html (`欢迎 ${name}`) 
+  $("#welcome").html(`欢迎 ${name}`);
   // 渲染用户名
   if (user.user_pic !== null) {
     $(".layui-nav-img").attr("src", user.user_pic).show();
@@ -35,16 +48,16 @@ const renderAvatar = (user) => {
 // 获取用户信息
 getUserinfo();
 // 实现退出功能
-$('.back').on('click',e=>{
-    console.log(11);
-    layui.layer.confirm(
-        "确定退出登录？",
-        { icon: 3, title: "" },
-        function (index) {
-            // 清空本地存储里面的 token
-            localStorage.removeItem("token");
-            // 重新跳转到登录页面
-            location.href = "/login.html";
-        }
-    );
-})
+$(".back").on("click", (e) => {
+  console.log(11);
+  layui.layer.confirm(
+    "确定退出登录？",
+    { icon: 3, title: "" },
+    function (index) {
+      // 清空本地存储里面的 token
+      localStorage.removeItem("token");
+      // 重新跳转到登录页面
+      location.href = "/login.html";
+    }
+  );
+});
