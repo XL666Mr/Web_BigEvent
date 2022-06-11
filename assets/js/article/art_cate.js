ee368c5333016a89a3e54f9cfaab48c5a3be0e89
XL_Mr <643158351@qq.com>
2022-06-11 20:07:24 +0800

    添加类别

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -13,5 +13,33 @@ $(function () {
       },
     });
   };
+// 点击按钮，弹出添加列表弹框
+let indexAdd = null
+  $("#btnAddCate").click(() => {
+    indexAdd = layer.open({
+      type: 1,
+      area: ["500px", "250px"],
+      title: "添加文章分类",
+      content: $("#dialog-add").html(),
+    });
+  });
+
+  // 添加文章分类
+  $('body').on('submit','#form-add',function(e){
+    // console.log(11);
+    e.preventDefault();
+    $.ajax({
+      type:'POST',
+      url:'/my/article/addcates',
+      data:$(this).serialize(),
+      success:res=>{
+        if(res.status !== 0) return layer.msg('新增分类失败')
+        layer.msg('新增分类成功')
+        initArtCateList()
+        layer.close(indexAdd)
+      }
+    })
+  })
   initArtCateList();
+
 });
